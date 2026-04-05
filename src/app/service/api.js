97c1d4dca2e3eadfa95cd5b4dc201f9fc7717853
Bhagesh <bhageshniloor159@gmail.com>
2026-04-05 17:18:43 +0530

    just cheking 2

--- src/app/service/api.js
+++ src/app/service/api.js
@@ -1,6 +1,6 @@
 const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY
 async function getWetherData(city) {
-    console.log(apiKey)
+    // console.log(apiKey)
     let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
 
     return data.json()
